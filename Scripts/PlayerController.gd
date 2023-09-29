fe43ef31df1ed3c388ac1019e2b3b361fb0014a5
danilomacb <danilo.bakun@gmail.com>
2023-09-29 11:08:52 -0300

    tilemap created and implementing shoot

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,22 +1,49 @@
 extends CharacterBody2D
 
-@export var _moveSpeed: float
+@export var moveSpeed: float
+
+@onready var timer = $Timer
+
+var playerArrow = load("res://Prefabs/PlayerArrow.tscn")
+
+var canShoot = true
 
 func _physics_process(delta):
 	HorizontalMove()
 	VerticalMove()
+	Shoot()
 	move_and_slide()
 
 func HorizontalMove():
 	var directionX = Input.get_axis("MoveLeft", "MoveRight")
 	if directionX:
-		velocity.x = directionX * _moveSpeed
+		velocity.x = directionX * moveSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, _moveSpeed)
+		velocity.x = move_toward(velocity.x, 0, moveSpeed)
 
 func VerticalMove():
 	var directionY = Input.get_axis("MoveUp", "MoveDown")
 	if directionY:
-		velocity.y = directionY * _moveSpeed
+		velocity.y = directionY * moveSpeed
 	else:
-		velocity.y = move_toward(velocity.y, 0, _moveSpeed)
+		velocity.y = move_toward(velocity.y, 0, moveSpeed)
+		
+func Shoot():
+	if Input.is_action_pressed("Shoot") && canShoot:
+		canShoot = false
+		timer.start()
+		
+		var instantiatedArrow = playerArrow.instantiate()
+		instantiatedArrow.global_position = global_position
+		
+		var toMouse = get_global_mouse_position() - global_position
+		toMouse = toMouse.normalized()
+		
+		var t = instantiatedArrow.transform.looking_at(toMouse)
+		instantiatedArrow.transform = t
+		
+		get_tree().root.add_child(instantiatedArrow)
+		instantiatedArrow.add_constant_force(toMouse * 300)
+
+func _on_timer_timeout():
+	canShoot = true
