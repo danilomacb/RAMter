981046d4ae4c4ede174ba86d914562823ade5a63
danilomacb <danilo.bakun@gmail.com>
2023-10-02 13:27:59 -0300

    spawning enemies in all directions

--- Scripts/EnemySpawnManager.gd
+++ Scripts/EnemySpawnManager.gd
@@ -1,15 +1,45 @@
 extends Node
 
+@onready var camera = get_node("/root/Game/Player/Camera2D")
+@onready var timer = $Timer
+
 var enemy = load("res://Prefabs/Enemy.tscn")
-var player = load("res://Prefabs/Player.tscn")
-@onready var cameraPos = get_node("/root/Game/Player/Camera2D").global_position
 
 var width = ProjectSettings.get_setting("display/window/size/viewport_width")
 var height = ProjectSettings.get_setting("display/window/size/viewport_height")
 
-func _ready():
-	var pos = Vector2(cameraPos.x, cameraPos.y - height / 2 - 10)
+var rng = RandomNumberGenerator.new()
+var canSpawnEnemy = true
+
+const margin = 5
+
+func _process(delta):
+	if !canSpawnEnemy:
+		return
+	
+	canSpawnEnemy = false
+	timer.start()
+	
+	var randomScreenSide = rng.randi_range(0, 3)
+	var pos = Vector2.ZERO
+	
+	match randomScreenSide:
+		0:
+			var randomScreenPos = rng.randi_range((width / 2) * -1, width / 2)
+			pos = Vector2(randomScreenPos, camera.global_position.y - height / 2 - margin)
+		1:
+			var randomScreenPos = rng.randi_range((width / 2) * -1, width / 2)
+			pos = Vector2(randomScreenPos, camera.global_position.y + height / 2 + margin)
+		2:
+			var randomScreenPos = rng.randi_range((height / 2) * -1, height / 2)
+			pos = Vector2(camera.global_position.x - (width / 2) - margin, randomScreenPos)
+		3:
+			var randomScreenPos = rng.randi_range((height / 2) * -1, height / 2)
+			pos = Vector2(camera.global_position.x + (width / 2) + margin, randomScreenPos)
+		
 	var instantiatedEnemy = enemy.instantiate()
 	instantiatedEnemy.global_position = pos
-	
 	add_child(instantiatedEnemy)
+
+func _on_timer_timeout():
+	canSpawnEnemy = true
