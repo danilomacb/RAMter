963a78ff595436ca7fa0707f476059081e4d6868
danilomacb <danilo.bakun@gmail.com>
2023-10-02 10:57:10 -0300

    EnemySpawnManager created

---
+++ Scripts/EnemySpawnManager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var enemy = load("res://Prefabs/Enemy.tscn")
+var player = load("res://Prefabs/Player.tscn")
+@onready var cameraPos = get_node("/root/Game/Player/Camera2D").global_position
+
+var width = ProjectSettings.get_setting("display/window/size/viewport_width")
+var height = ProjectSettings.get_setting("display/window/size/viewport_height")
+
+func _ready():
+	var pos = Vector2(cameraPos.x, cameraPos.y - height / 2 - 10)
+	var instantiatedEnemy = enemy.instantiate()
+	instantiatedEnemy.global_position = pos
+	instantiatedEnemy.moveSpeed = 100
+	
+	add_child(instantiatedEnemy)
