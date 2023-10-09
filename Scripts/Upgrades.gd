b40e256e5a42ab3ea67b35c95432a15916e12444
danilomacb <danilo.bakun@gmail.com>
2023-10-09 12:46:57 -0300

    reverse shot implemented

--- Scripts/Upgrades.gd
+++ Scripts/Upgrades.gd
@@ -4,26 +4,30 @@ enum UpgradeType {Multishot, ReverseShot}
 
 var playerArrow = load("res://Prefabs/PlayerArrow.tscn")
 
+var base_angle_diff = PI/12
+
 func InstantiateArrows(playerPos, bowPos, game, mousePos):
-	var base_angle_diff = PI/12
-	
 	for i in range(Globals.UpgradeMultiShotCounter + 1):
-		var instantiatedArrow = playerArrow.instantiate()
+		SetArrowDirAndRotation(i, playerPos, bowPos, game, mousePos)
+		
+	for i in range(Globals.UpgradeReverseShotCounter):
+		SetArrowDirAndRotation(i, playerPos, bowPos, game, playerPos + (playerPos - mousePos))
 
-		var angle = atan2(mousePos.y - playerPos.y, mousePos.x - playerPos.x)
-		var angleDif = base_angle_diff * (i - Globals.UpgradeMultiShotCounter / 2)
+func SetArrowDirAndRotation(i, playerPos, bowPos, game, mousePos):
+	var instantiatedArrow = playerArrow.instantiate()
 
-		instantiatedArrow.direction = Vector2(cos(angle + angleDif), sin(angle + angleDif))
-		instantiatedArrow.rotation = angle + angleDif
-		instantiatedArrow.global_position = bowPos
+	var angle = atan2(mousePos.y - playerPos.y, mousePos.x - playerPos.x)
+	var angleDif = base_angle_diff * (i - Globals.UpgradeMultiShotCounter / 2)
 
-		game.add_child(instantiatedArrow)
+	instantiatedArrow.direction = Vector2(cos(angle + angleDif), sin(angle + angleDif))
+	instantiatedArrow.rotation = angle + angleDif
+	instantiatedArrow.global_position = bowPos
+
+	game.add_child(instantiatedArrow)
 
 func AddUpgrade(upgradeType):
 	match (upgradeType):
 		UpgradeType.Multishot:
 			Globals.UpgradeMultiShotCounter += 1
-			print("multishot: " + str(Globals.UpgradeMultiShotCounter))
 		UpgradeType.ReverseShot:
 			Globals.UpgradeReverseShotCounter += 1
-			print("reverse: " + str(Globals.UpgradeReverseShotCounter))
