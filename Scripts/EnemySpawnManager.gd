dae5983cde040a377245f156f3eb111481e39dbc
danilomacb <danilo.bakun@gmail.com>
2023-10-10 08:44:26 -0300

    spawning enemys randomly

--- Scripts/EnemySpawnManager.gd
+++ Scripts/EnemySpawnManager.gd
@@ -3,7 +3,7 @@ extends Node
 @onready var camera = get_node("/root/Game/Player/Camera2D")
 @onready var timer = $Timer
 
-var enemy = load("res://Prefabs/Enemy.tscn")
+var enemys: Array[PackedScene] = [load("res://Prefabs/Enemy.tscn"), load("res://Prefabs/Tiilibra.tscn")]
 
 var width = ProjectSettings.get_setting("display/window/size/viewport_width")
 var height = ProjectSettings.get_setting("display/window/size/viewport_height")
@@ -14,12 +14,20 @@ var canSpawnEnemy = true
 const margin = 5
 
 func _process(delta):
+	SpawnEnemys()
+
+func SpawnEnemys():
 	if !canSpawnEnemy:
 		return
 	
 	canSpawnEnemy = false
 	timer.start()
 	
+	var instantiatedEnemy = GetRandomEnemy().instantiate()
+	instantiatedEnemy.global_position = GetRandomEnemyPos()
+	add_child(instantiatedEnemy)
+
+func GetRandomEnemyPos() -> Vector2:
 	var randomScreenSide = rng.randi_range(0, 3)
 	var pos = Vector2.ZERO
 	
@@ -36,10 +44,16 @@ func _process(delta):
 		3:
 			var randomScreenPos = rng.randi_range((height / 2) * -1, height / 2)
 			pos = Vector2(camera.global_position.x + (width / 2) + margin, randomScreenPos)
-		
-	var instantiatedEnemy = enemy.instantiate()
-	instantiatedEnemy.global_position = pos
-	add_child(instantiatedEnemy)
+			
+	return pos
+
+func GetRandomEnemy() -> PackedScene:
+	var randomEnemyIndex = rng.randi_range(0, 99)
+	
+	if randomEnemyIndex < 94:
+		return enemys[0]
+	
+	return enemys[1]
 
 func _on_timer_timeout():
 	canSpawnEnemy = true
