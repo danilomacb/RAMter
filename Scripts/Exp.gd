9441e82f6193c1cc604cc1f9c42345d9015d76e6
danilomacb <danilo.bakun@gmail.com>
2023-10-15 09:15:06 -0300

    exp magnet

--- Scripts/Exp.gd
+++ Scripts/Exp.gd
@@ -7,6 +7,8 @@ extends Area2D
 @onready var expCollectedSfx: AudioStreamPlayer2D = get_node("/root/Game/ExpCollectedSfx")
 
 var expIncrementor: int
+var goToPlayer: bool = false
+var player: CharacterBody2D
 
 func _ready():
 	match expType:
@@ -26,8 +28,19 @@ func _ready():
 			sprite2D.texture = crystalsTextures[3]
 			expIncrementor = 9999
 
+func _process(delta):
+	if !goToPlayer:
+		return
+	
+	global_position = global_position.move_toward(player.global_position, delta * 150)
+
 func _on_body_entered(body: CharacterBody2D):
 	if body.is_in_group("Player"):
 		body.get_node("PlayerExp").AddExp(expIncrementor)
 		expCollectedSfx.play()
 		queue_free()
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player"):
+		player = body
+		goToPlayer = true
